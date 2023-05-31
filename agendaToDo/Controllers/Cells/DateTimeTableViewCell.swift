c4ab748a36b0eb658d45c2434617b5e4b66904d6
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-05-31 15:22:05 -0300

    Change: Finaliando tela de criação de tasks

---
+++ agendaToDo/Controllers/Cells/DateTimeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateTimeTableViewCell.swift
+//  agendaToDo
+//
+//  Created by Marcel Leite de Farias on 31/05/23.
+//
+
+import UIKit
+
+class DateTimeTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var dateTimeTextField: UITextField!
+    
+}
---
+++ agendaToDo/Controllers/Cells/TaskDescriptionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskDescriptionTableViewCell.swift
+//  agendaToDo
+//
+//  Created by Marcel Leite de Farias on 31/05/23.
+//
+
+import UIKit
+
+class TaskDescriptionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var textDescriptionTextField: UITextField!
+    
+}
